6cb5ec70177a960fa54c2955db15aac6f5dfa3b2
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-10 13:34:01 -0500

    page-25

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -16,25 +16,17 @@ global $avia_config;
 
 				<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
 
-                    <?php
-                    /* Run the loop to output the posts.
-                    * If you want to overload this in a child theme then include a file
-                    * called loop-page.php and that will be used instead.
-                    */
-
-                    $avia_config['size'] = avia_layout_class( 'main' , false) == 'entry_without_sidebar' ? '' : 'entry_with_sidebar';
-                    get_template_part( 'includes/loop', 'page' );
-                    ?>
-
+                
 				<!--end content-->
+				<div>weqfret</div>
 				</main>
 
 				
-jhgkhk
+
 			</div><!--end container-->
 
 		</div><!-- close default .container_wrap element -->
-		<div>weqfret</div>
+		
 
 
 
