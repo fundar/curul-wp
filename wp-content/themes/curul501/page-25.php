993934f36ce87640c67d3171959f7a8add86dc74
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-09 20:32:11 -0500

    page-25

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -13,9 +13,9 @@ global $avia_config;
 		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
 
 			<div class='container'>
-                                    dsfrgreyrtey
+                                   
 				<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
-
+          
                     <?php
                     /* Run the loop to output the posts.
                     * If you want to overload this in a child theme then include a file
@@ -25,9 +25,24 @@ global $avia_config;
                     $avia_config['size'] = avia_layout_class( 'main' , false) == 'entry_without_sidebar' ? '' : 'entry_with_sidebar';
                     get_template_part( 'includes/loop', 'page' );
                     ?>
-
+				<div class="main_color container_wrap fullsize" id="after_layer_slider_1">
+					<div class="container">
+						<div class="template-page content  twelve alpha units">
+						       <div class="post-entry post-entry-type-page post-entry-25">
+							       <div class="entry-content-wrapper clearfix">
+									<div class="flex_column av_one_full first  avia-builder-el-1  el_after_av_revolutionslider  avia-builder-el-no-sibling  ">
+										<section itemtype="http://schema.org/CreativeWork" itemscope="itemscope" class="av_textblock_section">
+											<div itemprop="text" class="avia_textblock "><p>Click here to add your own text</p></div>
+										</section>
+									</div>
+							       </div>
+						       </div>
+						</div>
+					</div>
+				</div><!--fin iniciativas-->
 				<!--end content-->
 				</main>
+
 
 				<?php
 
