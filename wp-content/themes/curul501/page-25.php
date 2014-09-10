085d40bda36a0a88d559744abb575a57aebbe01e
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-10 16:21:49 -0500

    rev slider

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -11,27 +11,38 @@ global $avia_config;
 	 ?>
 
 		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
-		        <div id="rev_slider_1_1_wrapper" class="rev_slider_wrapper fullscreen-container" style="background-color: rgb(233, 233, 233); padding: 0px; position: absolute; max-height: none; overflow: visible; left: 0px; width: 1642px; height: 534px;">
-			<?php putRevSlider("carrusel-home","homepage") ?>
-			</div>
-
 
 			<div class='container'>
 
-			
+				<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
 
-                
+                    <?php
+                    /* Run the loop to output the posts.
+                    * If you want to overload this in a child theme then include a file
+                    * called loop-page.php and that will be used instead.
+                    */
+
+                    $avia_config['size'] = avia_layout_class( 'main' , false) == 'entry_without_sidebar' ? '' : 'entry_with_sidebar';
+                    get_template_part( 'includes/loop', 'page' );
+                    ?>
+<div id="rev_slider_1_1_wrapper" class="rev_slider_wrapper fullscreen-container" style="background-color: rgb(233, 233, 233); padding: 0px; position: absolute; max-height: none; overflow: visible; left: 0px; width: 1642px; height: 534px;">
+			<?php putRevSlider("carrusel-home","homepage") ?>
+			</div>
 				<!--end content-->
-				
 				</main>
 
-				
+				<?php
+
+				//get the sidebar
+				$avia_config['currently_viewing'] = 'page';
+				get_sidebar();
+
+				?>
 
 			</div><!--end container-->
 
 		</div><!-- close default .container_wrap element -->
-		
 
 
 
-<?php get_footer(); ?>
+<?php get_footer(); ?>	
