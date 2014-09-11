af5a23b1b5dd4888419fba3091220b81fe0a4ec8
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-11 18:24:27 -0500

    corrigiendo estructura home

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -15,7 +15,7 @@ global $avia_config;
 			<div class='container'>
 
 				<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>
-
+                   
                     <?php
                     /* Run the loop to output the posts.
                     * If you want to overload this in a child theme then include a file
@@ -26,35 +26,38 @@ global $avia_config;
                     get_template_part( 'includes/loop', 'page' );
                     ?>
 					
-						     <div class="template-page content  twelve alpha units">
+						     
 							     <div class="post-entry post-entry-type-page post-entry-25">
 								    <div class="entry-content-wrapper clearfix">
-									     <div class="flex_column av_one_full first  avia-builder-el-1  el_after_av_revolutionslider  avia-builder-el-no-sibling ">
+									     <div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first ">
 										    INICIATIVAS
-									     </div>
-								     </div>
+									     </div><!--fin iniciativas-->									     
+									     <div class="flex_column av_one_third avia-builder-el-2 el_after_av_two_third el_before_av_section avia-builder-el-last ">
+							
+						    
+
+					                                       <?php
+						
+										//get the sidebar
+										$avia_config['currently_viewing'] = 'page';
+										get_sidebar();
+						
+										?>
+										
+										<!--end content-->
+										
+						
+										<?php
+						
+										//get the sidebar
+										$avia_config['currently_viewing'] = 'page';
+										get_sidebar();
+						
+										?>
+						                             </div>
+								   </div>
 							     </div>
-						     </div>
-					   <!--fin iniciativas-->
-					    <?php
-
-				//get the sidebar
-				$avia_config['currently_viewing'] = 'page';
-				get_sidebar();
-
-				?>
-				
-				<!--end content-->
-			        </main> 
-
-				<?php
-
-				//get the sidebar
-				$avia_config['currently_viewing'] = 'page';
-				get_sidebar();
-
-				?>
-
+                                </main> 
 			</div><!--end container-->
 
 		</div><!-- close default .container_wrap element -->
