756d22ff9d301107cb8692f320cd7bbb4e896386
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-12 14:10:20 -0500

    home

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -8,14 +8,7 @@ global $avia_config;
 	<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
 	    <div class='container'>
 		<main class='template-page content  <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'page'));?>>              
-		<?php
-		/* Run the loop to output the posts.
-		* If you want to overload this in a child theme then include a file
-		* called loop-page.php and that will be used instead.
-		*/
-		$avia_config['size'] = avia_layout_class( 'main' , false) == 'entry_without_sidebar' ? '' : 'entry_with_sidebar';
-		get_template_part( 'includes/loop', 'page' );
-		?>			
+			
 			<div class="flex_column av_one_full first  avia-builder-el-1  el_after_av_revolutionslider  avia-builder-el-no-sibling  ">
 				<div class="container">
 					<div class="template-page content nine alpha units">
