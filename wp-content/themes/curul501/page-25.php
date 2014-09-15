671121b400b08e28d8fadf398ef1a330303d51e7
CeciliaMadrid <lya.madrid@gmail.com>
2014-09-15 17:51:35 -0500

    page-25 sidebar

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -29,7 +29,7 @@ global $avia_config;
 					</div><!--fin iniciativas-->	
 					<aside class="sidebar sidebar_right three alpha units" itemtype="https://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
                                                  <div class="inner_sidebar extralight-border">
-				                   <?php get_sidebar( $name ); ?> 
+				                  
 					         </div>
 					</aside>
 					
