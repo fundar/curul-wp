b4c2926914d83e5d42c6566c2bc4f49dbcd92aef
CeciliaMadrid <lya.madrid@gmail.com>
2014-11-12 21:45:14 -0600

    ajustes estilo en inic recientes y status tiempo

--- wp-content/themes/curul501/page-25.php
+++ wp-content/themes/curul501/page-25.php
@@ -25,6 +25,47 @@ global $avia_config;
 											<!--Inicio fecga y resumen-->
 											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first topTop">
 												<div class="post_date">
+													<span>13</span>
+													Feb, 2014
+												</div>
+												<div class="entry-content">
+													<p class="resemen-recientes-iniciativas">
+														Dar autonomia a los organos garantes federal y estatales, dar facultad al IFAI para promover acciones
+														de inconstitucionalidad y ampliar sujetos obligados (sindicatos, partidos politicos, fideicomisios publicos).
+													</p>
+												</div>
+											</div><!--fin fecha y resumen-->
+											<div class="flex_column av_one_third avia-builder-el-2 el_after_av_two_third avia-builder-el-last topTop leftRI">
+												<div class="col-status">
+													<div class="datos">
+													Status													
+													<div class="temporizador"><img src="wp-content/themes/curul501/images/barra-temporizador.png"></div>
+													</div>													
+												</div> 
+												<div class="col-status-1">
+													<div class="datos">
+													Votaci&oacute;n final													
+													</div>
+													<div class="votos-oficiles">
+														130
+													</div>
+												</div>
+												<div class="col-status-2">
+													<div class="datos">
+												         Propuesta por:
+													<div class="photo-avatar"><img src="wp-content/themes/curul501/images/avatar-m-42x42.png"></div>
+													</div>
+												</div>													
+											</div>											
+										</div>
+									</div>
+							</article><!--fin iniciativas-->
+				 <!--Inicio iniciaiva--><article class="post type-post status-publish format-standard hentry post-entry post-entry-type-standard post-parity-odd single-small pleca-29193E">
+									<div class="entry-content-wrapper clearfix">
+										<div class="entry-content-wrapper clearfix">
+											<!--Inicio fecga y resumen-->
+											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first topTop">
+												<div class="post_date">
 													<span>03</span>
 													Feb, 2014
 												</div>
@@ -59,11 +100,11 @@ global $avia_config;
 											</div>											
 										</div>
 									</div>
-							</article>							
+							</article><!--fin iniciativas-->							
 						    </div>
 					     </div>
-					</div><!--fin iniciativas-->					
-				</div>					     
+					</div><!--fin content-->					
+				</div><!--fin ccontainer_wrap -->					     
 		</main>
 		
                
