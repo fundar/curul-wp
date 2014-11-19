ac91bf15b307ade3b46fac5f5a69006fcea983c3
CeciliaMadrid <lya.madrid@gmail.com>
2014-11-19 03:06:28 -0600

    select filter en cpt iniciativas

--- wp-content/themes/curul501/single-iniciativa.php
+++ wp-content/themes/curul501/single-iniciativa.php
@@ -60,7 +60,7 @@
 		
 			<div class='container template-blog template-single-blog '>
 
-				<main class="content units av-content-small alpha" role="main">
+				<main class="content units av-content-small alpha cpt-iniciativa" role="main">
 				        <header class="entry-content-header">
 						<h1 itemprop="headline" class="post-title entry-title">
 							Phasellus viverra eros vel sem blandit, quis scelerisque neque malesuada
