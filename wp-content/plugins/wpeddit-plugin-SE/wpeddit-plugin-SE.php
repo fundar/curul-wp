049a8618122df3729057955d2b23805546725089
josechajob <josecha>
2014-11-19 16:37:56 -0600

    bug
    bug

--- wp-content/plugins/wpeddit-plugin-SE/wpeddit-plugin-SE.php
+++ wp-content/plugins/wpeddit-plugin-SE/wpeddit-plugin-SE.php
@@ -747,7 +747,7 @@ function wpeddit_hot_comments($posts){
 add_filter( 'the_content', 'wpeddit_content_filter', 20 );
 function wpeddit_content_filter( $content ) {
     
-	if(is_single(){
+	if(is_single()){
     $newcontent = epic_reddit_voting();
 	$content = $newcontent . $content;
 	}
