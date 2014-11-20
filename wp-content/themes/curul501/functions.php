5908f581de14bf4a175b08de69ee53ed8eee8017
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-11-20 14:22:35 -0600

    print error log

--- wp-content/themes/curul501/functions.php
+++ wp-content/themes/curul501/functions.php
@@ -63,6 +63,9 @@ add_action( 'init', 'create_post_type_iniciativas' );
 
 
 function redirect_xmlrpc_to_custom_post_type ($data, $postarr) {
+	error_log(print_r($data,true));
+	error_log(print_r($postarr,true));
+	
 	foreach($data["custom_fields"] as $key => $value) {
 		if($data["custom_fields"][$key]["key"] == "wp_post_type") {
 			$p2_custom_post_type = $data["custom_fields"][$key]["value"];
