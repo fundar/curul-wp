60a103efa55e4629fde39060b1a1271c01a3f7b5
CeciliaMadrid <lya.madrid@gmail.com>
2014-11-24 15:42:23 -0600

    archive

--- wp-content/themes/curul501/archive-representante.php
+++ wp-content/themes/curul501/archive-representante.php
@@ -5,87 +5,116 @@
 	 * get_header is a basic wordpress function, used to retrieve the header.php file in your theme directory.
 	 */
 	 get_header();
-	
-		
-		$showheader = true;
-		if(avia_get_option('frontpage') && $blogpage_id = avia_get_option('blogpage'))
-		{
-			if(get_post_meta($blogpage_id, 'header', true) == 'no') $showheader = false;
-		}
-		
-	 	if($showheader)
-	 	{
-			echo avia_title(array('title' => avia_which_archive()));
-		}
+
 	?>
-
 		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
+		        <!-- inicio mapa de representantes-->
+		       <div id="av_section_1" class="avia-section main_color avia-section-default avia-no-border-styling avia-bg-style-scroll avia-builder-el-0 avia-builder-el-no-sibling av-minimum-height av-minimum-height-100 container_wrap sidebar_right" style="background-color: #f4f4f4; ">
+				<div class="container">
+					Mapa
+				</div>
+                        </div>
+                        <!-- fin mapa de representantes-->
+			 <!-- titulo-->
+			<div class="container top60">
+			<h1 class="entry-title-yellow">Integrantes de la Camara</h1>
+			<div class="line-amarilla"> </div>
+		        </div>
+			<!-- fin de titulo-->
+<!--Inicio filtros iniciativas -->
+		<div class="container box-menu">
+			<div class="search-table">
+				<div id="filter">
+				       <select class="sorter-tema sort" name="category">
+					       <option value="1">Tema</option>
+					       <option value="2">Tema 2</option>							
+				       </select>
+			       </div>
+			       <div id="filter">				
+				       <select class="sorter-proponente sort" name="category">
+					       <option value="2">Proponente(s)</option>
+					       <option value="2">Opcion 2</option>														
+				       </select>
+			       </div>
+			       <div id="filter">				
+				       <select class="sorter-partido sort" name="category">
+					       <option value="1">Partido</option>
+					       <option value="2">PRI</option>														
+				       </select>
+			       </div>				
+			</div>
+		</div>
+<!-- Fin filtros iniciativas -->		
 
-			<div class='container template-blog '>
+		<div class='container_wrap container_wrap_first main_color fullsize'>
 
-				<main class='content <?php avia_layout_class( 'content' ); ?> units' <?php avia_markup_helper(array('context' => 'content','post_type'=>'post'));?>>
+			<div class='container'>
 
-                    <div class="category-term-description">
-                        <?php echo term_description(); ?>
-                    </div>
+				<main class='template-page content  av-content-full alpha units'>
+				<?php if (have_posts()) : ?>
+                                <?php while (have_posts()) : the_post(); ?>
+				 <!--Inicio iniciaiva--><article class="post type-post status-publish format-standard hentry post-entry post-entry-type-standard post-parity-odd single-small pleca-624070">
+									<div class="entry-content-wrapper clearfix">
+										<div class="entry-content-wrapper clearfix">
+											<!--Inicio fecga y resumen-->
+											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first topTop">
+												<div class="post_date">
+													<span>13</span>
+													Feb, 2014
+												</div>
+												<div class="entry-content">
+													 <p class="resemen-recientes-iniciativas titulo-<?php the_ID(); ?>">
+			                                                                                 <a class="iniciativas-home" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
+												         </p>
+													 <?php the_excerpt(); ?> 
+                          
+												</div>
+											</div><!--fin fecha y resumen-->
+											<div class="flex_column av_one_third avia-builder-el-2 el_after_av_two_third avia-builder-el-last topTop leftRI top12">
+												<div class="col-status">
+													<div class="datos">
+													Status													
+													<div class="temporizador"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/barra-temporizador.png" /></div>
+													</div>													
+												</div> 
+												<div class="col-status-1">
+													<div class="datos">
+													Votaci&oacute;n final													
+													</div>
+													<div class="votos-oficiles">
+														130
+													</div>
+												</div>
+												<div class="col-status-2">
+													<div class="datos">Propuesta por:</div>
+													<div class="photo-avatar"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/avatar-m-42x42.png"></div>
+													<div class="logo-partido"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/18px-PRI.png">
+															<span>PRI</span>
+													
+													</div>
+												</div>													
+											</div>
+											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first">
+												<div class="in-box-share">
+												<?php avia_social_share_links(); ?>
+												</div>
+											</div>
+											<div class="vta-curul">
+												<span>Votaci&oacute;n en Curul 501</span>
+												
+												
+												
+												
+												
+											</div>
 
-                    <?php
-                    $avia_config['blog_style'] = apply_filters('avf_blog_style', avia_get_option('blog_style','multi-big'), 'archive');
-                    if($avia_config['blog_style'] == 'blog-grid')
-                    {
-                        global $posts;
-                        $post_ids = array();
-                        foreach($posts as $post) $post_ids[] = $post->ID;
-
-                        if(!empty($post_ids))
-                        {
-                            $atts   = array(
-                                'type' => 'grid',
-                                'items' => get_option('posts_per_page'),
-                                'columns' => 3,
-                                'class' => 'avia-builder-el-no-sibling',
-                                'paginate' => 'yes',
-                                'use_main_query_pagination' => 'yes',
-                                'custom_query' => array( 'post__in'=>$post_ids, 'post_type'=>get_post_types() )
-                            );
-
-                            $blog = new avia_post_slider($atts);
-                            $blog->query_entries();
-                            echo "<div class='entry-content-wrapper'>".$blog->html()."</div>";
-                        }
-                        else
-                        {
-                            get_template_part( 'includes/loop', 'index' );
-                        }
-                    }
-                    else
-                    {
-                        /* Run the loop to output the posts.
-                        * If you want to overload this in a child theme then include a file
-                        * called loop-index.php and that will be used instead.
-                        */
-
-                        $more = 0;
-                        get_template_part( 'includes/loop', 'index' );
-                    }
-                    ?>
-
-				<!--end content-->
+											
+										</div>
+									</div>
+							</article><!--fin iniciativas-->
+							<?php endwhile; endif; ?>
 				</main>
-
-				<?php
-
-				//get the sidebar
-				$avia_config['currently_viewing'] = 'blog';
-				get_sidebar();
-
-				?>
-
 			</div><!--end container-->
-
 		</div><!-- close default .container_wrap element -->
-
-
-
 
 <?php get_footer(); ?>
