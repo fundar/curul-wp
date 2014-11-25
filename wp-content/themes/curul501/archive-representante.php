08be67974385ac0d13393e9362998b116e4b4cac
CeciliaMadrid <lya.madrid@gmail.com>
2014-11-24 20:18:29 -0600

    archive y css custom

--- wp-content/themes/curul501/archive-representante.php
+++ wp-content/themes/curul501/archive-representante.php
@@ -45,6 +45,7 @@
 			</div>
 		</div>
 <!-- Fin filtros iniciativas -->		
+<!-- Fin filtros iniciativas -->		
 
 		<div class='container_wrap container_wrap_first main_color fullsize'>
 
@@ -53,65 +54,34 @@
 				<main class='template-page content  av-content-full alpha units'>
 				<?php if (have_posts()) : ?>
                                 <?php while (have_posts()) : the_post(); ?>
-				 <!--Inicio iniciaiva--><article class="post type-post status-publish format-standard hentry post-entry post-entry-type-standard post-parity-odd single-small pleca-624070">
-									<div class="entry-content-wrapper clearfix">
-										<div class="entry-content-wrapper clearfix">
+				 <!--Inicio iniciaiva--><article class="post type-post status-publish format-standard hentry post-entry post-entry-type-standard post-parity-odd single-small">
+									
+										
 											<!--Inicio fecga y resumen-->
-											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first topTop">
-												<div class="post_date">
-													<span>13</span>
-													Feb, 2014
+											<div class="flex_column av_four_fifth  first ">
+												<div class="post_foto">
+													<?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?>
 												</div>
 												<div class="entry-content">
-													 <p class="resemen-recientes-iniciativas titulo-<?php the_ID(); ?>">
+													 
 			                                                                                 <a class="iniciativas-home" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a>
-												         </p>
-													 <?php the_excerpt(); ?> 
+												      
+													
                           
 												</div>
 											</div><!--fin fecha y resumen-->
-											<div class="flex_column av_one_third avia-builder-el-2 el_after_av_two_third avia-builder-el-last topTop leftRI top12">
-												<div class="col-status">
-													<div class="datos">
-													Status													
-													<div class="temporizador"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/barra-temporizador.png" /></div>
-													</div>													
-												</div> 
-												<div class="col-status-1">
-													<div class="datos">
-													Votaci&oacute;n final													
-													</div>
-													<div class="votos-oficiles">
-														130
-													</div>
-												</div>
-												<div class="col-status-2">
-													<div class="datos">Propuesta por:</div>
-													<div class="photo-avatar"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/avatar-m-42x42.png"></div>
-													<div class="logo-partido"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/18px-PRI.png">
-															<span>PRI</span>
-													
-													</div>
-												</div>													
-											</div>
-											<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first">
-												<div class="in-box-share">
+											<div class="flex_column av_one_fifth vota-rep">
+												
+																								
+											</div>											
+										
+									
+							</article><!--fin iniciativas-->
+							<div class="flex_column av_two_third first avia-builder-el-0 el_before_av_one_third avia-builder-el-first">
+												<div class="share-social-representante ">
 												<?php avia_social_share_links(); ?>
 												</div>
-											</div>
-											<div class="vta-curul">
-												<span>Votaci&oacute;n en Curul 501</span>
-												
-												
-												
-												
-												
-											</div>
-
-											
-										</div>
-									</div>
-							</article><!--fin iniciativas-->
+							</div>
 							<?php endwhile; endif; ?>
 				</main>
 			</div><!--end container-->
