fdcc67cf10805abda1b96273db9d89dacd33aa78
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-11-26 16:54:02 -0600

    Mapa representantes jQuery #debug

--- wp-content/themes/curul501/single-representante.php
+++ wp-content/themes/curul501/single-representante.php
@@ -234,11 +234,11 @@
 		<?php if($district == "") { ?>
 			<script src="<?php echo get_stylesheet_directory_uri() ?>/js/estados.geojson.js" type="text/javascript"></script>
 			<script src="<?php echo get_stylesheet_directory_uri() ?>/js/init-map.js" type="text/javascript"></script>
-			<script type="text/javascript">$(document).ready( function () { setMap("<?php echo $circum;?>"); });</script>
+			<script type="text/javascript"> jQuery(document).ready( function () { setMap("<?php echo $circum;?>"); }); </script>
 		<?php } else { ?>
 			<script src="<?php echo get_stylesheet_directory_uri() ?>/js/estado-<?php echo $state;?>.geojson.js" type="text/javascript"></script>
 			<script src="<?php echo get_stylesheet_directory_uri() ?>/js/init-map-dist.js" type="text/javascript"></script>
-			<script type="text/javascript">$(document).ready( function () { setMap("<?php echo $state;?>", "<?php echo $district;?>"); });</script>
+			<script type="text/javascript"> jQuery(document).ready( function () { setMap("<?php echo $state;?>", "<?php echo $district;?>"); }); </script>
 		<?php } ?>
 		
 		<div id="map" style="width: 235px; height:323px;"></div>
