bf6939739894384ac11e9ed85df12d88b46b37de
CeciliaMadrid <lya.madrid@gmail.com>
2014-12-03 00:05:56 -0600

    agregando log in/aut a item menu

--- wp-content/themes/curul501/functions.php
+++ wp-content/themes/curul501/functions.php
@@ -348,3 +348,13 @@ function getParameterValueGET() {
 		return "";
 	}
 }
+
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
+function add_login_logout_link($items, $args) {
+        ob_start();
+        wp_loginout('index.php');
+        $loginoutlink = ob_get_contents();
+        ob_end_clean();
+        $items .= '<li>'. $loginoutlink .'</li>';
+    return $items;
+}
