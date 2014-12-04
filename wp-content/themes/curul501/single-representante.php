517df06e4c70880041fa87be8068daeca6f7bbdd
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-12-04 14:03:51 -0600

    representante - tipo de elección sidebar

--- wp-content/themes/curul501/single-representante.php
+++ wp-content/themes/curul501/single-representante.php
@@ -103,10 +103,6 @@
 					</header>
 					<div class="entry-content no-voto" itemprop="text">
 						<ul class="lista-iniciativas">
-							<li class="bullet-arrow">Tipo de elecci&oacute;n
-								<p><?php echo get_post_meta($post->ID, 'wp_election_type', true); ?></p>
-							</li>
-							
 							<li class="bullet-arrow">Comisiones a las que pertenece
 								<p><?php echo str_replace('|', ", ", get_post_meta($post->ID, 'wp_commissions', true)); ?></p>
 							</li>
@@ -204,6 +200,10 @@
 					<br/><br/>
 				</li>
 				
+				<li class="bullet-arrow">Tipo de elecci&oacute;n
+					<p><?php echo get_post_meta($post->ID, 'wp_election_type', true); ?></p>
+				</li>
+							
 				<li class="correo-sb">
 					<?php if($district == "") { ?>
 						Circunscripción: <?php echo $circum;?>
