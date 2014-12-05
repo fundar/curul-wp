f2666348d8fcc81075d2f5d6103ee0c0e625ecd3
Kevin Martinez <n4utilius@gmail.com>
2014-12-05 17:22:16 -0600

    cambios graficas

--- wp-content/themes/curul501/js/scripts/app.js
+++ wp-content/themes/curul501/js/scripts/app.js
@@ -296,7 +296,7 @@ run.pieChart = function(votos, urlBase){
       .attr('height', function (d) { return yScale.rangeBand();  })
       .attr('width', function (d) { return xScale(d.x); })
       .on('mouseover', function (d) {
-          jQuery("#tooltip").one("mousemove", function( event ) {
+          jQuery("body").one("mousemove", function( event ) {
               d3.select('#tooltip')
                   .style('left', ( event.pageX - 115) + 'px')
                   .style('top', ( event.pageY - 150) + 'px')
@@ -307,7 +307,6 @@ run.pieChart = function(votos, urlBase){
           })
 
       }).on('mouseout', function () {
-          jQuery("#tooltip").one("mousemove", function( event ) { })
           d3.select('#tooltip').classed('hidden', true);
       })
 
