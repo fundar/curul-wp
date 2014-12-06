4b1a75b4c2a4e6c7f61271317cf7fbe0a10f6fcd
josechajob <josecha>
2014-12-05 21:42:01 -0600

    if post  type

--- wp-content/themes/curul501/functions.php
+++ wp-content/themes/curul501/functions.php
@@ -719,3 +719,9 @@ function remove_admin_bar() {
 	  show_admin_bar(false);
 	}
 }
+
+function is_post_type($type){
+    global $wp_query;
+    if($type == get_post_type($wp_query->post->ID)) return true;
+    return false;
+}
