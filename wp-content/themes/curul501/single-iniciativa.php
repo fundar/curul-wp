65a522cb5629ee5eff7890b6810042f274f7014b
Kevin Martinez <n4utilius@gmail.com>
2014-12-08 11:59:54 -0600

    cambios graficas

--- wp-content/themes/curul501/single-iniciativa.php
+++ wp-content/themes/curul501/single-iniciativa.php
@@ -69,6 +69,7 @@
 
 
 ?>
+	<script src="<?php echo get_stylesheet_directory_uri() ?>/js/libs/angular.min.js" type="text/javascript"></script>
 	<script src="<?php echo get_stylesheet_directory_uri() ?>/js/libs/d3.v3.min.js" type="text/javascript"></script>
 
 	<script src="<?php echo get_stylesheet_directory_uri() ?>/js/scripts/app.js" type="text/javascript"></script>
