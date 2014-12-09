05ad6568fc39b03fce51a4845fcff102dba91093
CeciliaMadrid <lya.madrid@gmail.com>
2014-12-09 07:45:09 -0600

    archive-representantes manita

--- wp-content/themes/curul501/archive-iniciativa.php
+++ wp-content/themes/curul501/archive-iniciativa.php
@@ -174,7 +174,7 @@
 													</div>
 													<div class="votos-oficiles">
 													</div>
-													<div class="hands-vote">
+													<div class="hands-vote-inic">
 									                                <ul>
 													        <li class="hand-up"><?php echo $decode[8]['favor']; ?></li>
 													        <li class="hand-down"><?php echo $decode[8]['contra']; ?></li>
