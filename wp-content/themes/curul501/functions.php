2f7b166a6fd5307db6e0f04927dd991dca553ba0
josechajob <josecha>
2015-02-25 03:17:30 -0600

    test doble

--- wp-content/themes/curul501/functions.php
+++ wp-content/themes/curul501/functions.php
@@ -637,7 +637,7 @@ function getTemas() {
 
 	$temas = array(
 		array("name" => "transparencia", "slug" => "transparencia"),
-		array("name" => "rendición de cuentas",  "slug" => "rendicion-de-cuentas"),
+		array("name" => "rendicióón de cuentas",  "slug" => "rendicion-de-cuentas"),
 		array("name" => "anticorrupción", "slug" => "anticorrupcion"),
 		array("name" => "publicidad oficial", "slug" => "publicidad-oficial"),
 		array("name" => "parlamento abierto", "slug" => "parlamento-abierto"),
