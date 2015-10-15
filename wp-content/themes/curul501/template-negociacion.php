3922dc6961488aee5217ecf388d2de69815cc32e
CecilaMadrid <lya.madrid@gmail.com>
2015-10-15 12:05:36 -0500

    corrigieno img

--- wp-content/themes/curul501/template-negociacion.php
+++ wp-content/themes/curul501/template-negociacion.php
@@ -159,7 +159,7 @@
 	<div class="container">
 		<div class="flex_column av_three_fourth flex_column_div first main_color">
                         <div class="post-img">
-                           <img src="<?php echo get_home_url(); ?>/wp-content/themes/curul501/images/exige-th-1.png">
+                           <img src="<?php echo get_home_url(); ?>/wp-content/themes/curul501/images/exige-num-1.png">
                         </div>
 			<?php the_title( '<h3 class="titulo-art-nego">', '</h3>' ); ?>	
 		</div>
@@ -170,7 +170,7 @@
                 <p class="tit-hero">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus ultrices arcu, eget luctus purus molestie quis #NegociacionPresupuestaria.</p>
 		<div class="flex_column av_two_fifth first el_after_av_section el_before_av_three_fifth avia-builder-el-first">
                         <div class="tw-img">
-                           <img src="http://curul501.org/wp-content/uploads/2015/10/1.png">
+                           <img src="http://curul501.org/wp-content/uploads/2015/10/exige-th-1.png">
                         </div>
 		</div>
 
