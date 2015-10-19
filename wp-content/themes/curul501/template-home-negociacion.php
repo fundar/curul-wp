a421cae5667f967bd377b94b4aec9b7cc87d0a80
CecilaMadrid <lya.madrid@gmail.com>
2015-10-19 13:48:05 -0500

    Estilo sección video home negociacion
    eliminado carácter extraño en css

--- wp-content/themes/curul501/template-home-negociacion.php
+++ wp-content/themes/curul501/template-home-negociacion.php
@@ -145,7 +145,7 @@
                         width: 35%;
 		}
 		.page-id-4978 > #wrap_all > #main > #cabecera-negociacion > .container > .content > .videoneg > a {display: none;}
-		#∫video-micrositioNegociacion {
+		#video-micrositioNegociacion {
                         border-bottom: 1px solid #beb1c4 !important;
                         padding: 40px 0 20px;
                 }
